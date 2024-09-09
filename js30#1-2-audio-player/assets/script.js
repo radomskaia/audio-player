b24697a27a9705920dfa3974d6198644b21a90e4
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-09-09 09:06:43 +0700

    feat: add audio, make playback and switching songs

--- js30#1-2-audio-player/assets/script.js
+++ js30#1-2-audio-player/assets/script.js
@@ -1,9 +1,9 @@
-let album = [
 const SONG_DIRECTION = {
     NEXT: 'next',
     PREVIOUS: 'prev',
 }
 
+const SECONDS_IN_MINUTES = 60;
 
 const album = [
     {
@@ -19,6 +19,9 @@ const album = [
         coverSrc: 'assets/image/covers/02 Nuki Sabotage.jpeg',
     },
 ]
+
+let playerStatus, songNumber, audio, rewindBtn, playBtn, forwardBtn, coverImg, artistName, songName, nextSong, prevSong,
+    progressCurrentTime, songDuration, progressBar;
 
 function createDOMElement(tagName, parentElement, ...classList) {
     const newEl = document.createElement(tagName);
@@ -54,11 +57,46 @@ const forwardBtn = createDOMElement('img', buttonBox, 'rewindBtn');
 forwardBtn.alt = 'play/rewind button';
 forwardBtn.src = 'assets/image/icons/icons8-fast-forward-64.png';
 
+function init() {
+    const body = document.body;
+    const container = createDOMElement('div', body, 'container');
+    const playerBox = createDOMElement('div', container, 'playerBox');
+    const coverBox = createDOMElement('div', playerBox, 'coverBox');
+    coverImg = createDOMElement('img', coverBox, 'coverImg');
+    coverImg.alt = 'cover';
+    const songBox = createDOMElement('div', playerBox, 'songBox');
+    // const songBar = createDOMElement('div', playerBox, 'songBar');
+    const nameBox = createDOMElement('div', songBox, 'nameBox');
+    artistName = createDOMElement('h1', nameBox, 'artistName');
+    songName = createDOMElement('h2', nameBox, 'songName');
+    const buttonBox = createDOMElement('div', songBox, 'buttonBox');
+    rewindBtn = createDOMElement('img', buttonBox, 'rewindBtn', 'btn');
+    playBtn = createDOMElement('img', buttonBox, 'playBtn', 'btn');
+    forwardBtn = createDOMElement('img', buttonBox, 'rewindBtn', 'btn');
+
+    audio = new Audio();
+    console.log(audio)
+    playerStatus = false;
+    songNumber = 0
+    audio.src = album[songNumber].audioSrc;
+    coverImg.src = album[songNumber].coverSrc;
+    artistName.textContent = album[songNumber].artist;
+    songName.textContent = album[songNumber].songName;
+
+    rewindBtn.alt = 'play/rewind button';
+    rewindBtn.src = 'assets/image/icons/icons8-rewind-64.png';
+    playBtn.alt = 'play/pause button';
+    playBtn.src = 'assets/image/icons/icons8-play-64.png'
+    forwardBtn.alt = 'play/rewind button';
+    forwardBtn.src = 'assets/image/icons/icons8-fast-forward-64.png';
+
     nextSong = changeSongNumber.bind('next');
     prevSong = changeSongNumber.bind('prev');
 }
 
 function playPauseMusic() {
+    if (playerStatus) audio.pause();
+    else audio.play();
     playerStatus = !playerStatus;
     playBtn.src = playerStatus ? 'assets/image/icons/icons8-pause-64.png' : 'assets/image/icons/icons8-play-64.png';
 }
@@ -70,9 +108,12 @@ function changeSongNumber() {
     if (songNumber + 1 > album.length) songNumber = 0;
     if (songNumber < 0) songNumber = album.length - 1;
 
+    audio.src = album[songNumber].audioSrc;
     coverImg.src = album[songNumber].coverSrc;
     artistName.textContent = album[songNumber].artist;
     songName.textContent = album[songNumber].songName;
+    playerStatus = false;
+    playPauseMusic()
 }
 
 function keyRewind(e) {
@@ -87,8 +128,10 @@ function keyRewind(e) {
 
 
 
+init()
 
 playBtn.addEventListener('click', playPauseMusic)
 forwardBtn.addEventListener('click', nextSong);
 rewindBtn.addEventListener('click', prevSong);
 document.addEventListener('keydown', keyRewind);
+audio.addEventListener('ended', nextSong)
