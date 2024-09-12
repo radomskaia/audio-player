f7dcf420629d594f0d09ae5ab3a1b42c10a5cf58
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-09-12 23:15:54 +0700

    refactor: clean code

--- js30#1-2-audio-player/assets/script.js
+++ js30#1-2-audio-player/assets/script.js
@@ -2,13 +2,11 @@ const SONG_DIRECTION = {
     NEXT: 'next',
     PREVIOUS: 'prev',
 }
-
 const SECONDS_IN_MINUTES = 60;
-const HALF_OF_SECOND = 500;
 const MILLISECONDS_IN_SECOND = 1000;
+const HALF_OF_SECOND = 500;
 const REWIND_SPEED = 30;
 const TO_PERCENT = 100;
-
 const VOLUME = {
     MUTE: 0,
     LOW: 0.25,
@@ -45,7 +43,8 @@ const album = [
 ]
 
 
-let isPlay, songNumber, audio, coverImg, artistName, songName, playBtn, rewindBtn, forwardBtn, progressCurrentTime,
+let isPlay, currentSongIndex = 0, audio = new Audio(), coverImg, artistName, songName, playBtn, rewindBtn, forwardBtn,
+    progressCurrentTime,
     songDuration, progressBar, progressPopOver, textPopOver, rewindTime, isMouseMove, pressedTime, isPressed, nextSong,
     prevSong, isUp, arrowPressed, repeatBtn, volumeBtn, volumeValue = 0.5, repeatValue = 0, isShuffle,
     shuffleSet = new Set, bodyEl;
@@ -59,8 +58,8 @@ function createDOMElement(tagName, parentElement, ...classList) {
     return newEl
 }
 
-
 function init() {
+    // build HTML Structure
     bodyEl = document.body;
     const container = createDOMElement('main', bodyEl, 'container');
     const footer = createDOMElement('footer', bodyEl, 'footer');
@@ -102,16 +101,6 @@ function init() {
     forwardBtn = createDOMElement('img', buttonBox, 'rewindBtn', 'btn');
     volumeBtn = createDOMElement('img', buttonBox, 'optionsBtn');
 
-    audio = new Audio();
-    console.log(audio)
-    isPlay = false;
-    songNumber = 0
-
-    renderSongData ()
-    showTime(true)
-    changeRepeat()
-    changeVolume()
-
     repeatBtn.alt = 'repeat selection button';
     rewindBtn.alt = 'play/rewind button';
     rewindBtn.src = 'assets/image/icons/icons8-rewind-64.png';
@@ -121,8 +110,14 @@ function init() {
     forwardBtn.src = 'assets/image/icons/icons8-fast-forward-64.png';
     volumeBtn.alt = 'volume selection button';
 
-    nextSong = changeSong.bind('next');
-    prevSong = changeSong.bind('prev');
+    nextSong = switchSong.bind('next');
+    prevSong = switchSong.bind('prev');
+
+
+    renderSongData()
+    showTime(true)
+    changeRepeat()
+    changeVolume()
 
     // Event Listeners
     repeatBtn.addEventListener('click', () => {
@@ -136,27 +131,21 @@ function init() {
     playBtn.addEventListener('click', playPauseMusic)
     forwardBtn.addEventListener('click', nextSong);
     rewindBtn.addEventListener('click', prevSong);
-    document.addEventListener('keydown', keyDownRewind);
-    document.addEventListener('keyup', keyUpRewind);
+    document.addEventListener('keydown', keyDown);
+    document.addEventListener('keyup', keyUp);
     audio.addEventListener('timeupdate', showTime);
     audio.addEventListener('canplay', () => showTime(false))
     audio.addEventListener('progress', bufferedTime);
     progressBarClickZone.addEventListener('mousedown', () => isMouseMove = true);
 
     progressBarClickZone.addEventListener('mouseup', (e) => {
-        isMouseMove = false;
         rewindSong(e);
-        progressPopOver.classList.remove('progressPopOver_active');
-        audio.currentTime = rewindTime;
+        stopRewind()
     });
 
     progressBarClickZone.addEventListener('mouseleave', () => {
         if (!isMouseMove) return;
-
-        audio.currentTime = rewindTime;
-        progressPopOver.classList.remove('progressPopOver_active');
-
-        isMouseMove = false
+        stopRewind()
     })
 
     progressBarClickZone.addEventListener('mousemove', (e) => {
@@ -175,122 +164,115 @@ function playPauseMusic() {
 function randomSong() {
     if (!isShuffle) return false
     if (shuffleSet.size === album.length) shuffleSet.clear()
-    shuffleSet.add(songNumber);
+    shuffleSet.add(currentSongIndex);
     let setSize = shuffleSet.size;
     do {
-        songNumber = Math.floor(Math.random() * album.length);
-        shuffleSet.add(songNumber)
+        currentSongIndex = Math.floor(Math.random() * album.length);
+        shuffleSet.add(currentSongIndex)
     } while (setSize === shuffleSet.size);
-
     return true
 }
 
-function changeSong() {
+
+function switchSong() {
     if (!randomSong()) {
-        if (this === SONG_DIRECTION.NEXT) songNumber++;
-        else if (this === SONG_DIRECTION.PREVIOUS) songNumber--;
-        if (songNumber + 1 > album.length) songNumber = 0;
-        if (songNumber < 0) songNumber = album.length - 1;
+        if (this === SONG_DIRECTION.NEXT) currentSongIndex++;
+        else if (this === SONG_DIRECTION.PREVIOUS) currentSongIndex--;
+
+        if (currentSongIndex + 1 > album.length) currentSongIndex = 0;
+        else if (currentSongIndex < 0) currentSongIndex = album.length - 1;
     }
     renderSongData()
-
-    isPlay = false;
     playPauseMusic()
 }
 
 function renderSongData() {
-    bodyEl.style.backgroundImage = `linear-gradient(to bottom right, rgba(22, 66, 60, 0.8), rgba(22, 66, 60, 0.8)), url('${album[songNumber].coverSrc}')`;
-    audio.src = album[songNumber].audioSrc;
-    coverImg.src = album[songNumber].coverSrc;
-    artistName.textContent = album[songNumber].artist;
-    songName.textContent = album[songNumber].songName;
+    bodyEl.style.backgroundImage = `linear-gradient(to bottom right, rgba(22, 66, 60, 0.8), rgba(22, 66, 60, 0.8)), url('${album[currentSongIndex].coverSrc}')`;
+    audio.src = album[currentSongIndex].audioSrc;
+    coverImg.src = album[currentSongIndex].coverSrc;
+    artistName.textContent = album[currentSongIndex].artist;
+    songName.textContent = album[currentSongIndex].songName;
+    isPlay = false;
 }
 
-function keyDownRewind(e) {
-    // console.log('до', arrowPressed);
+function keyDown(e) {
     if ((e.key !== 'ArrowLeft' && e.key !== 'ArrowRight')
         || (e.key !== arrowPressed && arrowPressed !== undefined)) {
         if (isPressed) {
-            stopKeyRewind(true)
-            document.removeEventListener('keydown', keyDownRewind);
-            console.log('keydown removed', isUp);
+            stopRewind(true, 'key')
+            document.removeEventListener('keydown', keyDown);
         }
         return
     }
-    // if (isUp) return;
-    if (!isPressed) pressedTime = e.timeStamp;
-    isPressed = true;
+    if (!pressedTime) pressedTime = e.timeStamp;
+    isPressed = e.timeStamp - pressedTime > HALF_OF_SECOND;
     arrowPressed = e.key;
-    if (e.timeStamp - pressedTime > HALF_OF_SECOND) {
+    if (isPressed) {
         if (e.key === 'ArrowLeft') {
-            keyRewind(e, SONG_DIRECTION.PREVIOUS);
+            rewindSong(e, SONG_DIRECTION.PREVIOUS);
         } else if (e.key === 'ArrowRight') {
-            keyRewind(e, SONG_DIRECTION.NEXT)
+            rewindSong(e, SONG_DIRECTION.NEXT)
         }
     }
 }
 
-function stopKeyRewind(bool) {
-    isPressed = false;
-    progressPopOver.classList.remove('progressPopOver_active');
-    console.log('PopOver removed');
-    audio.currentTime += rewindTime;
-    rewindTime = 0;
-    isUp = bool;
-}
+function keyUp(e) {
+    pressedTime = undefined;
 
-function keyUpRewind(e) {
     if (e.key !== 'ArrowLeft' && e.key !== 'ArrowRight' && e.code !== 'Space') return;
     if (isUp) {
         if (e.key === arrowPressed) {
-            arrowPressed = undefined;
             isUp = false;
-            document.addEventListener('keydown', keyDownRewind);
+            document.addEventListener('keydown', keyDown);
+            arrowPressed = undefined;
         }
         return;
     }
 
-    if ((e.key === 'ArrowLeft' || e.key === 'ArrowRight') && (e.timeStamp - pressedTime > HALF_OF_SECOND)) {
-        stopKeyRewind(false)
-        arrowPressed = undefined;
-        return
-    }
-
-    arrowPressed = undefined;
-    isPressed = false;
-    if (e.key === 'ArrowLeft') {
+    if ((e.key === 'ArrowLeft' || e.key === 'ArrowRight') && isPressed) {
+        stopRewind(false, 'key')
+    } else if (e.key === 'ArrowLeft') {
         prevSong()
     } else if (e.key === 'ArrowRight') {
         nextSong()
     } else if (e.code === 'Space') {
         playPauseMusic()
     }
+
+    arrowPressed = undefined;
 }
 
-function keyRewind(e, direction) {
-    rewindTime = (e.timeStamp - pressedTime) / MILLISECONDS_IN_SECOND * REWIND_SPEED;
-    console.log(rewindTime);
-    rewindTime = direction === 'next' ? rewindTime : -rewindTime;
-    progressPopOver.classList.add('progressPopOver_active');
-    console.log('popover add');
-    let updatedCurrenTime = audio.currentTime + rewindTime;
-    if (updatedCurrenTime < 0) updatedCurrenTime = 0;
-    if (updatedCurrenTime > audio.duration) updatedCurrenTime = audio.duration;
-    textPopOver.textContent = getTimeString(updatedCurrenTime);
-    const offsetPercent = updatedCurrenTime / audio.duration * TO_PERCENT;
-    progressPopOver.style.left = `${offsetPercent}%`
-    progressBar.style.setProperty('--progress-width', `${offsetPercent}%`)
+function rewindSong(e, direction) {
+    let offsetX;
+    if (!direction) {
+        const offsetX = e.clientX - progressBar.getBoundingClientRect().left;
+        const timePercent = progressBar.clientWidth / offsetX;
+        rewindTime = audio.duration / timePercent;
+    } else {
+        rewindTime = (e.timeStamp - pressedTime - HALF_OF_SECOND) / MILLISECONDS_IN_SECOND * REWIND_SPEED;
+        rewindTime = direction === 'next' ? rewindTime : -rewindTime;
+        rewindTime = audio.currentTime + rewindTime;
+        if (rewindTime < 0) rewindTime = 0;
+        if (rewindTime > audio.duration) rewindTime = audio.duration;
+    }
 
-}
-
-function rewindSong(e) {
-    const offsetX = e.clientX - progressBar.getBoundingClientRect().left;
-    const timePercent = progressBar.clientWidth / offsetX;
-    rewindTime = audio.duration / timePercent;
     progressPopOver.classList.add('progressPopOver_active');
+    const offsetPercent = rewindTime / audio.duration * TO_PERCENT;
     textPopOver.textContent = getTimeString(rewindTime);
-    progressBar.style.setProperty('--progress-width', `${rewindTime / audio.duration * TO_PERCENT}%`)
-    progressPopOver.style.left = `${offsetX}px`
+    progressPopOver.style.left = offsetX ? `${offsetX}px` : `${offsetPercent}%`;
+    progressBar.style.setProperty('--progress-width', `${offsetPercent}%`)
+}
+
+function stopRewind(bool, type) {
+    if (type === 'key') {
+        isPressed = false;
+        isUp = bool;
+    } else {
+        isMouseMove = false
+    }
+
+    progressPopOver.classList.remove('progressPopOver_active');
+    audio.currentTime = rewindTime;
 }
 
 function getTimeString(time) {
@@ -321,7 +303,7 @@ function changeRepeat() {
             audio.onended = nextSong;
             break;
         case REPEAT.ONE:
-            audio.onended = changeSong;
+            audio.onended = switchSong;
             break;
         case REPEAT.SHUFFLE:
             isShuffle = true;
